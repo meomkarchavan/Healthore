d2dc3973212a36a53ead27409d9f654c25d01720
Sanket Jadhav <chavanomkar245@gmail.com>
2020-03-06 17:06:41 +0530

    homepage again

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:heatlhore/screens/Home/Home.dart';
+import 'package:heatlhore/screens/Home/Profile.dart';
+import 'package:heatlhore/screens/Home/addPost.dart';
+import 'package:heatlhore/services/auth.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+ List<Widget> _widgetOptions = <Widget>[
+  Home(),
+  AddPost(),
+  Profile()
+];
+
+void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+      final AuthService _auth = AuthService();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      actions: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.people),
+              label: Text('Log Out'),
+            ),
+            
+          ],
+      ),
+      body: SafeArea(child: _widgetOptions[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          title: Text('Post'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.portrait),
+          title: Text('Profile'),
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    ),
+    );
+  }
+}
+
